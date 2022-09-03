34edc9fb3d6978d684deaf96ba7b8fde2d46f470
Ishtiaq <robin.pass36@gmail.com>
2022-09-04 04:56:37 +0600

    modal is start

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ const lodeAllNews = async () => {
 
 const setAllmenu = async () => {
     lodeAllNews();
+
     const data = await lodeAllNews();
 
     const menu = document.getElementById('all-menu');
@@ -14,7 +15,7 @@ const setAllmenu = async () => {
         const li = document.createElement("li");
         li.innerHTML = `<a onclick="updateNews(${news.category_id})" >${news.category_name}</a>
         
-        `
+        `;
         menu.appendChild(li);
         li.classList.add("mr-16")
     }
@@ -31,33 +32,55 @@ const updateNews = (id) => {
         .then((data) => displayPost(data.data));
 };
 
+// dynamic cetagory button click
 const displayPost = updates => {
     console.log(updates);
+
+    // start spinner or loader
+    // toggleSpinner(true);
     const updateNewsContainer = document.getElementById('new-news');
     updateNewsContainer.textContent = ``;
+
+
     updates.forEach(newNews => {
         // console.log(newNews);
         const newNewsDiv = document.createElement('div');
         newNewsDiv.innerHTML = `
         <div class="card card-side bg-base-100 shadow-xl mb-2.5">
-                    <figure><img src="${newNews.thumbnail_url}" /></figure>
+                    <img src="${newNews.thumbnail_url}"/>
                 <div class="card-body">
-                    <h2 class="card-title">The best fashion influencers to follow for sartorial inspiration</h2>
-                    <p>Click the button to watch on Jetflix app.</p>
+                    <h2 class="card-title font-bold text-2xl">${newNews.title}</h2>
+                    <P>${newNews.details.slice(0, 700)}</p>
                     <div class="flex avatar">
                 <div class="w-10" rounded-full">
-                    <img src="${newNews.image_url}"/>
+                    <img class="w-10 rounded-full" src="${newNews.image_url}"/>
                 </div>
-                    <p class="text-xl font-black ml-2">${newNews.author.name}jon</p>
-                    <p class="font-black">${newNews.total_view ? newNews.total_view : 'No Viwe founded'}M</p>
-                    <button class="btn btn-outline">Button</button>
+                    <p class="font-black ml-2">${newNews.author.name}</p>
+                    <p class="font-black"><i class="fa-regular fa-eye"></i> ${newNews.total_view ? newNews.total_view : 'No Viwe founded'}M</p>
+                    <button type="button" data-modal-toggle="defaultModal"><i class="text-xl font-bold fa-sharp fa-solid fa-arrow-right"></i></button>
+
+                    <button
+                        class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                        Toggle modal
+                    </button>
                 </div >
             </div >
         </div >
                 `;
         updateNewsContainer.appendChild(newNewsDiv);
+
     });
 }
 
-
 setAllmenu();
+
+// const toggleSpinner = isLoading => {
+//     const loaderSection = document.getElementById('loader');
+//     if (isLoading) {
+//         loaderSection.classList.remove('hidden');
+//     }
+//     else {
+//         loaderSection.classList.add('hidden');
+//     }
+// }
